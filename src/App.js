ecb146ef78e73b5993f3c75d069116f2e9ac0910
ajaybarath <ajaybarath3@gmail.com>
2021-11-11 16:57:10 +0530

    Added: redirect to bridgelanz on clicking of logo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  
-  
-  var title = "Hello from BridgeLabz";
-  
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      title: "Hello from BridgeLabz"
+    }
+    this.url = "https://www.bridgelabz.com/";
+  }
+
+  onClick = ($event) => {
+    console.log("image clicked", $event);
+    window.open(this.url, "_blank");
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" onClick={this.onClick}/>
         <p>
-          {title}
+          {this.state.title}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    )
+  }
+
 }
 
 export default App;
