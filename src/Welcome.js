fcfb83f9ce89fd2ec7d6bf2e954c173845e8e7b5
ajaybarath <ajaybarath3@gmail.com>
2021-11-11 18:39:31 +0530

    Added: welcome page

---
+++ src/Welcome.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class WelcomePage extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+            Welcome to home page
+        </h1>
+      </div>
+    )
+  }
+
+}
+
+export default WelcomePage;
