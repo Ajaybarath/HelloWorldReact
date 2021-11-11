d9d02435fd37dbb957397c8ab862de6b9d71f407
ajaybarath <ajaybarath3@gmail.com>
2021-11-11 17:12:11 +0530

    Added: onNameChange to reflect change in main page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: "Hello from BridgeLabz"
+      title: "Hello from BridgeLabz",
+      userName: ''
     }
     this.url = "https://www.bridgelabz.com/";
   }
 
-  onClick = ($event) => {
-    console.log("image clicked", $event);
+  onClick = (event) => {
+    console.log("image clicked", event);
     window.open(this.url, "_blank");
+  }
+
+  onNameChange = (event) => {
+    console.log("value is " , )
+    this.setState({userName: event.target.value})
   }
 
   render() {
@@ -25,6 +31,9 @@ class App extends React.Component {
         <p>
           {this.state.title}
         </p>
+        <h1>
+          Hello {this.state.userName}
+        </h1>
       </header>
     </div>
     )
